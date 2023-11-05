be0eee21d3a8f863ad3e6b9e6d815e5a4a2a59b7
José <caianajsb@gmail.com>
2023-11-05 13:34:12 -0300

    Added validation and open API

--- src/main/java/com/ecommerce/inventory/controller/InventoryController.java
+++ src/main/java/com/ecommerce/inventory/controller/InventoryController.java
@@ -6,11 +6,16 @@ package com.ecommerce.inventory.controller;
 
 import com.ecommerce.inventory.dto.InventoryDTO;
 import com.ecommerce.inventory.service.InventoryService;
+import jakarta.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,8 +38,20 @@ public class InventoryController {
     
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
-    public String updateInventories(@RequestBody List<InventoryDTO> inventories){
+    public String updateInventories(@RequestBody @Valid List<InventoryDTO> inventories){
         service.updateInventories(inventories);
         return "Inventories updated sucessfully";
+    }
+    
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
--- src/main/java/com/ecommerce/inventory/dto/InventoryDTO.java
+++ src/main/java/com/ecommerce/inventory/dto/InventoryDTO.java
@@ -1,5 +1,7 @@
 package com.ecommerce.inventory.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +14,8 @@ import lombok.NoArgsConstructor;
 public class InventoryDTO {
 
     private Long id;
+    @NotBlank
     private String skuCode;
+    @Min(0)
     private Integer quantity;
 }
